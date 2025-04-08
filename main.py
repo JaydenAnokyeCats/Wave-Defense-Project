0178cbe5cc7eddd50840a9ff3448b5b6d093a53d
Jayden Anokye <jaydenanokyecats@gmail.com>
2025-04-07 21:33:58 -0400

    progress push #2- image is on screen, moveable. .gitignore file installed to remove some annoying files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame 
 import sys
+from utility import load_image, load_images # Functions residing in the utility folder
 
 class Game: 
     def __init__(self):
@@ -8,34 +9,54 @@ class Game:
         pygame.display.set_caption('Defenders') # Creates window title
         
         self.screen = pygame.display.set_mode((900,700)) # Creates a window. Parameters are width(px) and height(px)
+        self.display = pygame.Surface((450,350))
         
         self.clock = pygame.time.Clock() # Helps restrict frame rate
         
-        self.display = pygame.Surface((900,700))
+        self.movement = [False, False]
         
-        self.img = pygame.image.load('xRXXModel-Creation.0012.png')
+        self.assets = {
+            'player': load_image('player_sprites/xRXXModel.0012.png')
+        }
+        
+        self.img_pos = [100, 200]
         
         # self.image_surface
         
         
-
         
     def run(self):
         running = True
         
         # Game Loop
         while running:
-            self.display.fill((0,200,0)) # Placeholder method for me setting a background
-            self.screen.blit(self.img,(0,0))
-
             
+            self.display.fill((14,111,222)) # Placeholder method for me setting a background
+            
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) #
+            
+            # Exit
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                     
+            # Movement
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
             
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size())) 
+            # 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
+            self.screen.blit(self.assets['player'], self.img_pos) # Blits the image, and sets the image position on the screen
+            
             pygame.display.update()
         
         
--- utility.py
+++ utility.py
@@ -1,4 +1,22 @@
 import pygame
 import os
 
-# This code
+# This code makes it easier to get images, as opposed to typing in the path the image is stored in to access it.
+
+Base_IMG_Path = 'Data/Assets/' # Base directory path where all image assets for the project are stored
+
+
+def load_image(path):
+    img = pygame.image.load(Base_IMG_Path + path).convert() # .convert() allows the image to be 'blitted' (drawn) onto the screen more effectively
+    img.set_colorkey((0,0,0))
+    return img
+
+
+
+def load_images(path):
+    images = []
+    for img_name in sorted(os.listdir(Base_IMG_Path + path)):
+        images.append(load_image(path + '/' + img_name))
+        # Name of desired path, slash to separate, name of the image
+    return images
+
